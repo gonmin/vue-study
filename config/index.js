305e691094efb4bf5d194dbcd06de442aa296fbf
gonmin <860282341.com>
2018-05-19 20:58:39 +0800

    revise

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://gonmin.github.io/vue-study/',
 
     /**
      * Source Maps
